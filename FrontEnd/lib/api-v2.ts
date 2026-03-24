454186c601c6b03e8d1b3af60a54be62093311b4
Martins_dev <mtz.martinss03@gmail.com>
2026-03-24 16:07:21 -0300

    Url Backend deploy

--- FrontEnd/lib/api-v2.ts
+++ FrontEnd/lib/api-v2.ts
@@ -2,7 +2,7 @@
 "use client";
 
 export const API_BASE_URL =
-  process.env.NEXT_PUBLIC_API_URL ?? "http://localhost:8080";
+  process.env.NEXT_PUBLIC_API_URL ?? "https://gestpro-backend-production.up.railway.app/";
 
 // ===================== Tipos =====================
 
